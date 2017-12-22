19e68d50a2591ef8aae6dacdcaa9a817ade67a85
rogowoi <nikita.rogovoy@gmail.com>
2017-12-22 15:55:42 +0200

    simple process

--- image_segm/image_segm/Form1.Designer.cs
+++ image_segm/image_segm/Form1.Designer.cs
@@ -28,12 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.srcPicBox = new System.Windows.Forms.PictureBox();
+            this.resPicBox = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.filteringToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.processToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.processSimpleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.processMediumToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.processManiacToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.srcPicBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resPicBox)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // srcPicBox
+            // 
+            this.srcPicBox.Location = new System.Drawing.Point(12, 24);
+            this.srcPicBox.Name = "srcPicBox";
+            this.srcPicBox.Size = new System.Drawing.Size(590, 613);
+            this.srcPicBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.srcPicBox.TabIndex = 0;
+            this.srcPicBox.TabStop = false;
+            // 
+            // resPicBox
+            // 
+            this.resPicBox.Location = new System.Drawing.Point(608, 24);
+            this.resPicBox.Name = "resPicBox";
+            this.resPicBox.Size = new System.Drawing.Size(590, 613);
+            this.resPicBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.resPicBox.TabIndex = 1;
+            this.resPicBox.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.filteringToolStripMenuItem,
+            this.processToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1210, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.openToolStripMenuItem.Text = "Open...";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.saveToolStripMenuItem.Text = "Save...";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // filteringToolStripMenuItem
+            // 
+            this.filteringToolStripMenuItem.Name = "filteringToolStripMenuItem";
+            this.filteringToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.filteringToolStripMenuItem.Text = "Filtering";
+            // 
+            // processToolStripMenuItem
+            // 
+            this.processToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.processSimpleToolStripMenuItem,
+            this.processMediumToolStripMenuItem,
+            this.processManiacToolStripMenuItem});
+            this.processToolStripMenuItem.Name = "processToolStripMenuItem";
+            this.processToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.processToolStripMenuItem.Text = "Process";
+            // 
+            // processSimpleToolStripMenuItem
+            // 
+            this.processSimpleToolStripMenuItem.Name = "processSimpleToolStripMenuItem";
+            this.processSimpleToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.processSimpleToolStripMenuItem.Text = "Process simple";
+            this.processSimpleToolStripMenuItem.Click += new System.EventHandler(this.processSimpleToolStripMenuItem_Click);
+            // 
+            // processMediumToolStripMenuItem
+            // 
+            this.processMediumToolStripMenuItem.Name = "processMediumToolStripMenuItem";
+            this.processMediumToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.processMediumToolStripMenuItem.Text = "Process medium";
+            this.processMediumToolStripMenuItem.Click += new System.EventHandler(this.processMediumToolStripMenuItem_Click);
+            // 
+            // processManiacToolStripMenuItem
+            // 
+            this.processManiacToolStripMenuItem.Name = "processManiacToolStripMenuItem";
+            this.processManiacToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.processManiacToolStripMenuItem.Text = "Process maniac";
+            this.processManiacToolStripMenuItem.Click += new System.EventHandler(this.processManiacToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1210, 660);
+            this.Controls.Add(this.resPicBox);
+            this.Controls.Add(this.srcPicBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.srcPicBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resPicBox)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox srcPicBox;
+        private System.Windows.Forms.PictureBox resPicBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem filteringToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem processToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem processSimpleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem processMediumToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem processManiacToolStripMenuItem;
     }
 }
 
--- image_segm/image_segm/Form1.cs
+++ image_segm/image_segm/Form1.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +20,163 @@ namespace image_segm
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        Bitmap InitialImage;
+        bool loaded = false;
+        private void process(Bitmap bm)
+        {
+            Image<Bgr, Byte> img = new Image<Bgr, Byte>(bm);
+            //Convert the image to grayscale and filter out the noise
+            UMat uimage = new UMat();
+            CvInvoke.CvtColor(img, uimage, ColorConversion.Bgr2Gray);
+
+            //use image pyr to remove noise
+            UMat pyrDown = new UMat();
+            CvInvoke.PyrDown(uimage, pyrDown);
+            CvInvoke.PyrUp(pyrDown, uimage);
+
+            //Image<Gray, Byte> gray = img.Convert<Gray, Byte>().PyrDown().PyrUp();
+
+            double cannyThreshold = 180.0;
+            double circleAccumulatorThreshold = 120;
+            CircleF[] circles = CvInvoke.HoughCircles(uimage, HoughType.Gradient, 2.0, 20.0, cannyThreshold, circleAccumulatorThreshold, 5);
+
+            double cannyThresholdLinking = 120.0;
+            UMat cannyEdges = new UMat();
+            CvInvoke.Canny(uimage, cannyEdges, cannyThreshold, cannyThresholdLinking);
+
+            LineSegment2D[] lines = CvInvoke.HoughLinesP(
+               cannyEdges,
+               1, //Distance resolution in pixel-related units
+               Math.PI / 45.0, //Angle resolution measured in radians.
+               20, //threshold
+               30, //min Line width
+               10); //gap between lines
+
+            List<Triangle2DF> triangleList = new List<Triangle2DF>();
+            List<RotatedRect> boxList = new List<RotatedRect>(); //a box is a rotated rectangle
+
+            using (VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint())
+            {
+                CvInvoke.FindContours(cannyEdges, contours, null, RetrType.List, ChainApproxMethod.ChainApproxSimple);
+                int count = contours.Size;
+                for (int i = 0; i < count; i++)
+                {
+                    using (VectorOfPoint contour = contours[i])
+                    using (VectorOfPoint approxContour = new VectorOfPoint())
+                    {
+                        CvInvoke.ApproxPolyDP(contour, approxContour, CvInvoke.ArcLength(contour, true) * 0.05, true);
+                        if (CvInvoke.ContourArea(approxContour, false) > 250) //only consider contours with area greater than 250
+                        {
+                            if (approxContour.Size == 3) //The contour has 3 vertices, it is a triangle
+                            {
+                                Point[] pts = approxContour.ToArray();
+                                triangleList.Add(new Triangle2DF(
+                                   pts[0],
+                                   pts[1],
+                                   pts[2]
+                                   ));
+                            }
+                            else if (approxContour.Size == 4) //The contour has 4 vertices.
+                            {
+                                #region determine if all the angles in the contour are within [80, 100] degree
+                                bool isRectangle = true;
+                                Point[] pts = approxContour.ToArray();
+                                LineSegment2D[] edges = PointCollection.PolyLine(pts, true);
+
+                                for (int j = 0; j < edges.Length; j++)
+                                {
+                                    double angle = Math.Abs(
+                                       edges[(j + 1) % edges.Length].GetExteriorAngleDegree(edges[j]));
+                                    if (angle < 80 || angle > 100)
+                                    {
+                                        isRectangle = false;
+                                        break;
+                                    }
+                                }
+                                #endregion
+
+                                if (isRectangle) boxList.Add(CvInvoke.MinAreaRect(approxContour));
+                            }
+                        }
+                    }
+                }
+            }
+
+            
+            Image<Bgr, Byte> Image = img.CopyBlank();
+            foreach (Triangle2DF triangle in triangleList)
+                Image.Draw(triangle, new Bgr(Color.DarkBlue), 2);
+            foreach (RotatedRect box in boxList)
+                Image.Draw(box, new Bgr(Color.DarkOrange), 2); 
+            Image<Bgr, Byte> circleImage = img.CopyBlank();
+            foreach (CircleF circle in circles)
+                Image.Draw(circle, new Bgr(Color.Brown), 2);
+
+            resPicBox.Image = Image.ToBitmap();
+           
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+
+            dlg.Title = "Open first image";
+            dlg.Filter = "Image Files|*.bmp;*.gif;*.jpg;*.jpeg;*.png|All files (*.*)|*.*";
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                srcPicBox.Image = new Bitmap(dlg.FileName);
+                InitialImage = new Bitmap(dlg.FileName);
+            }
+            loaded = true;
+            dlg.Dispose();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Images|*.png;*.bmp;*.jpg";
+            ImageFormat format = ImageFormat.Png;
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                string ext = System.IO.Path.GetExtension(sfd.FileName);
+                switch (ext)
+                {
+                    case ".jpg":
+                        format = ImageFormat.Jpeg;
+                        break;
+                    case ".bmp":
+                        format = ImageFormat.Bmp;
+                        break;
+                    case ".png":
+                        format = ImageFormat.Png;
+                        break;
+                }
+                resPicBox.Image.Save(sfd.FileName, format);
+            }
+        }
+
+        private void processSimpleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            process((Bitmap)srcPicBox.Image);
+        }
+
+        private void processMediumToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void processManiacToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
